03a8bae9b3df9b118816eb900db3d464275bb1c5
armando0598 <armando.leon@upr.edu>
2021-10-21 02:50:11 -0400

    Making change to main branch. Added a test form to test database

--- app/views.py
+++ app/views.py
@@ -1,19 +1,32 @@
 from app import app
 from app.db import *
+from flask import render_template, request, redirect
+import psycopg2
 
 
 @app.route('/')
 def home():
-    return 'Welcome to team AIEMs Database Semester Project'
+    return render_template("form.html")
 
 
-@app.route('/test')
+#Create a new user in the table
+@app.route('/submit-user-info', methods=['POST'])
 def create():
 
+    #Setup the DB
     db = Database
-    con = db.create_db_connection()
+    con = db.create_db_connection(db)
     cur = con.cursor()
-    cur.execute("CREATE TABLE accounts(user_id serial PRIMARY KEY,username VARCHAR ( 50 ), email VARCHAR ( 255 ))")
-    con.commit()
 
-    return "Table created"
+    #Get info from HTML form
+    us_name = request.form['us_name']
+    us_username = request.form['us_username']
+    us_password = request.form['us_password']
+
+    #Submit the information into the Database
+    cur.execute("""INSERT INTO "User"(us_name, us_username, us_password) VALUES(%s, %s, %s)""", (us_name, us_username, us_password))
+    con.commit()
+    cur.close()
+    con.close()
+
+    return redirect("form.html")
