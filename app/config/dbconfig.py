e47fb6c0d18e2a288db1789e135f099aba8e25ea
EmmanuelCHernandez <emmanuel.hernandez14@upr.edu>
2021-11-10 14:15:56 -0400

    [FIX] dbconfig now pulls from URI for easier changing

--- app/config/dbconfig.py
+++ app/config/dbconfig.py
@@ -1,6 +1,13 @@
+puri = "postgres://seqmujfcvmcqhh:91bad46d3e7e5df3ff53d48a41e27fd9ac827acae2b75d2eb12b9722f939e860@ec2-18-206-20-102.compute-1.amazonaws.com:5432/d5gkt64aa114el"
+filler, credentials = puri.split("postgres://")
+slice1, slice2 = credentials.split("@")
+username, password = slice1.split(":")
+host, slice3 = slice2.split(":")
+port, database = slice3.split("/")
+
 aiem_config = {
-    'dbname': 'd5gkt64aa114el',
-    'user': 'seqmujfcvmcqhh',
-    'host': 'ec2-18-206-20-102.compute-1.amazonaws.com',
-    'password': '91bad46d3e7e5df3ff53d48a41e27fd9ac827acae2b75d2eb12b9722f939e860'
+    'dbname': database,
+    'user': username,
+    'host': host,
+    'password': password
 }
