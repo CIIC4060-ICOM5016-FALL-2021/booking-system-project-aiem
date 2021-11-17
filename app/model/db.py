ead31ebeb61d6f666f91cd8f8377509a26ab1281
EmmanuelCHernandez <emmanuel.hernandez14@upr.edu>
2021-11-17 15:09:32 -0400

    [FIX] Touched up db connection files

--- app/model/db.py
+++ app/model/db.py
@@ -6,9 +6,10 @@ from app.config.dbconfig import aiem_config
 # Creates connection to database for easier legibility in DAO classes
 class Database:
     def __init__(self):
-        self.connection = psycopg2.connect("dbname=%s user=%s host=%s password=%s"
+        self.connection = psycopg2.connect("dbname=%s user=%s host=%s password=%s port=%s"
                                            % (aiem_config['dbname'], aiem_config['user'],
-                                              aiem_config['host'], aiem_config['password']))
+                                              aiem_config['host'], aiem_config['password'],
+                                              aiem_config['port']))
 
     def close(self):
         self.connection.close()
