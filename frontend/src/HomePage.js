77d9e2828c8438e7899d1c531704b627fa0c64e6
Ignacio Tampe <Igtampe@gmail.com>
2021-11-17 14:10:58 -0400

    Partial implementation of Login

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,60 +1,106 @@
-import React, {Component, useState} from 'react';
-import {Button, Divider, Form, Grid, Header, Modal, Segment, Tab} from 'semantic-ui-react';
+import React, { Component, useState } from 'react';
+import { Button, Divider, Form, Grid, Header, Modal, Segment, Tab } from 'semantic-ui-react';
+import Constants from './Constants'
+import Cookies from 'universal-cookie';
+import { Navigate } from 'react-router';
 
-
+const cookies = new Cookies();
 
 function HomePage() {
+ 
+    const [AuthRequest, SetAuthRequest] = useState({
+        "username": "",
+        "password": ""
+    });
+
+    const [loginInProgress, setLoginInProgress] = useState(false)
+    const [loginError, setLoginError] = useState(false)
+
     const [open, setOpen] = useState(false);
     console.log(open);
-    const handleChange = (event, newValue) => {
-        setOpen(true);
+
+    const handleLogin = (e) => {
+        console.log(AuthRequest.username)
+        setLoginInProgress(true)
+
+        //Grab the ID and pin and create a tiny itty bitty object
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(AuthRequest)
+        };
+
+        console.log(requestOptions.body);
+
+        fetch(Constants.ApiURL + "Auth/", requestOptions)
+            .then(response => {
+                setLoginInProgress(false);
+                if (!response.ok) { setLoginError(true) }
+                return response.json()
+            }).then(data => {
+                console.log(data)
+                if (data !== undefined && !loginError) {
+                    //We logged in, save a cookie, then let's get the heck out of here
+                    cookies.set('SessionID', data, { path: '/' })
+                    Navigate("/UserView", { replace: true })
+                }
+            })
     }
 
+    const handleChange = (event, newValue) => { setOpen(true); }
+
+
     return (<Segment><Header dividing textAlign="center" size="huge">Welcome to DB Demo</Header>
-            <Modal
-                centered={false}
-                open={open}
-                onClose={() => setOpen(false)}
-                onOpen={() => setOpen(true)}
-            >
-                <Modal.Header>Needs changing!</Modal.Header>
-                <Modal.Content>
-                    <Modal.Description>
-                        This is a modal but it serves to show how buttons and functions can be implemented.
-                    </Modal.Description>
-                </Modal.Content>
-                <Modal.Actions>
-                    <Button onClick={() => setOpen(false)}>OK</Button>
-                </Modal.Actions>
-            </Modal>
-            <Segment placeholder>
+        <Modal
+            centered={false}
+            open={open}
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+        >
+            <Modal.Header>Needs changing!</Modal.Header>
+            <Modal.Content>
+                <Modal.Description>
+                    This is a modal but it serves to show how buttons and functions can be implemented.
+                </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+                <Button onClick={() => setOpen(false)}>OK</Button>
+            </Modal.Actions>
+        </Modal>
+        <Segment placeholder>
 
-                <Grid columns={2} relaxed='very' stackable>
-                    <Grid.Column>
-                        <Form>
-                            <Form.Input
-                                icon='user'
-                                iconPosition='left'
-                                label='Username'
-                                placeholder='Username'
-                            />
-                            <Form.Input
-                                icon='lock'
-                                iconPosition='left'
-                                label='Password'
-                                type='password'
-                            />
-                            <Button content='Login' primary onClick={handleChange}/>
-                        </Form>
-                    </Grid.Column>
-                    <Grid.Column verticalAlign='middle'>
-                        <Button content='Sign up' icon='signup' size='big' onClick={handleChange}/>
-                    </Grid.Column>
-                </Grid>
+            <Grid columns={2} relaxed='very' stackable>
+                <Grid.Column>
+                    <Form>
+                        <Form.Input
+                            icon='user'
+                            iconPosition='left'
+                            label='Username'
+                            placeholder='Username'
+                            error={loginError}
+                            disabled={loginInProgress}
+                            onChange={(e) => { SetAuthRequest({ ...AuthRequest, "username": e.target.value }) }}
+                        />
+                        <Form.Input
+                            icon='lock'
+                            iconPosition='left'
+                            label='Password'
+                            type='password'
+                            error={loginError}
+                            disabled={loginInProgress}
+                            onChange={(e) => { SetAuthRequest({ ...AuthRequest, "password": e.target.value }) }}
+                        />
+                        <Button content='Login' primary onClick={handleLogin} />
+                    </Form>
+                </Grid.Column>
+                <Grid.Column verticalAlign='middle'>
+                    <Button content='Sign up' icon='signup' size='big' onClick={handleChange} />
+                </Grid.Column>
+            </Grid>
 
-                <Divider vertical>Or</Divider>
-            </Segment>
+            <Divider vertical>Or</Divider>
         </Segment>
+    </Segment>
     )
 }
 
