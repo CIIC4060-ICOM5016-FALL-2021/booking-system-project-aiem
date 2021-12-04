cf4ad33ceae5e8fdabe9739314c334787cf3283c
EmmanuelCHernandez <emmanuel.hernandez14@upr.edu>
2021-12-04 17:42:53 -0400

    [FEAT] added basic frontend view of full user schedule

--- frontend/src/Schedule.js
+++ frontend/src/Schedule.js
@@ -1,8 +1,9 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import {Calendar, momentLocalizer, Views } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
 import {Button, Card, Container, Modal} from "semantic-ui-react";
+import Constants from "./Constants";
 
 
 // Event {
@@ -14,15 +15,40 @@ import {Button, Card, Container, Modal} from "semantic-ui-react";
 // }
 
 
-function Schedule(){
-    const [dates, setDates] = useState([{
-        'title': 'Selection',
-        'allDay': false,
-        'start': new Date(moment.now()),
-        'end': new Date(moment.now())
-    }]);
+export default function Schedule(props){
+    const [dates, setDates] = useState(undefined);
     const [open, setOpen] = useState(false);
     const localizer = momentLocalizer(moment)
+
+    useEffect(() => {
+        if (props.user !== undefined) {
+            fetch(Constants.ApiURL + "users/" + props.user.us_id + "/schedule")
+                .then(response => {
+                    if (!response.ok) {
+                        setDates([{
+                            'title': 'Selection',
+                            'allDay': false,
+                            'start': new Date(moment.now()),
+                            'end': new Date(moment.now())
+                        }])
+                        return undefined
+                    }
+                    return response.json()
+                }).then(data => {
+                console.log(data)
+                if (data !== undefined) {
+                    let events = data.map( event => ({
+                        title: event.title,
+                        desc: event.desc,
+                        start: new Date(event.date + ' ' + event.start),
+                        end: new Date(event.date + ' ' + event.start),
+                        allDay: false
+                    }))
+                    setDates(events)
+                }
+            })
+        }
+    }, []);
 
     return <Container style={{ height: 800 }}><Calendar
         localizer={localizer}
@@ -38,4 +64,3 @@ function Schedule(){
 
 
 }
-export default Schedule;
